4b58303393632e1139fb76f872cd8f72313a38e4
Erick Nave <edub.rulez@gmail.com>
2014-02-18 10:35:54 -0500

    split angular controllers

---
+++ src/AngularJS.UIBootstrap/Scripts/app/controllers/login.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+// Google Analytics Collection APIs Reference:
+// https://developers.google.com/analytics/devguides/collection/analyticsjs/
+
+angular.module('app.controllers', [])
+
+    // Path: /login
+    .controller('LoginCtrl', ['$scope', '$location', '$window', function($scope, $location, $window) {
+            $scope.$root.title = 'AngularJS SPA | Sign In';
+
+            // TODO: Authorize a user
+            $scope.login = function() {
+                $location.path('/');
+                return false;
+            };
+
+            $scope.$on('$viewContentLoaded', function() {
+                $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+            });
+        }
+    ]);
